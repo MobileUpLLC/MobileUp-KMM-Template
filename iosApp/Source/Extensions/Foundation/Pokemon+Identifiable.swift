73003cf528a259d928baf42982d2acf7f0329091
Artur Artikov <artur.artikov@mobileup.ru>
2023-03-24 11:48:14 +0300

    Don't use value classes for PokemonId and PokemonTypeId. Simplify iOS sample.

--- iosApp/Source/Extensions/Foundation/Pokemon+Identifiable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Pokemon+Identifiable.swift
-//  iosApp
-//
-//  Created by Vladislav Grokhotov on 20.03.2023.
-//  Copyright © 2023 orgName. All rights reserved.
-//
-
-extension Pokemon: Identifiable {}
