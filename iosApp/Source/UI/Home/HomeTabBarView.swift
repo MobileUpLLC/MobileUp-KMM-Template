60fe40516876eed8deb040468d42b7f701a6180c
Vladislav Grokhotov <vlad@grokhotov.ru>
2023-04-18 22:52:01 +0300

    Add ability to present bottom sheet via  tabBar

--- iosApp/Source/UI/Home/HomeTabBarView.swift
+++ iosApp/Source/UI/Home/HomeTabBarView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Combine
 
 protocol HomeTabViewController: UIViewController {
     var homeTab: HomeTab { get }
@@ -20,97 +21,113 @@ struct HomeTabBarView: UIViewControllerRepresentable {
     var tabScreen: (HomeComponentChild) -> HomeTabViewController?
     var onTabSelected: (HomeTab) -> Void
     
-    private var stack: [Child<AnyObject, HomeComponentChild>] { tabsStack.value.items }
-    
-    func makeCoordinator() -> HomeTabBarCoordinator {
-        HomeTabBarCoordinator { tab in
-            onTabSelected(tab)
-        }
+    func makeUIViewController(context: Context) -> HomeTabBarController {
+        return HomeTabBarController(tabsStack: tabsStack, tabScreen: tabScreen, onTabSelected: onTabSelected)
     }
     
-    func makeUIViewController(context: Context) -> UITabBarController {
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = createTabViewControllers(context.coordinator)
-        tabBarController.delegate = context.coordinator
+    func updateUIViewController(_ navigationController: HomeTabBarController, context: Context) {}
+}
+
+class HomeTabBarController: UITabBarController, BottomSheetPresentable {
+    var transitionDelegate: BottomSheetTransitioningDelegate?
+    var canBottomSheetBeDismissed: Bool { true }
+    
+    private var stack: [HomeComponentChild] { tabsStack.value.items.compactMap { $0.instance } }
+    private let coordinator: HomeTabBarCoordinator
+    private var subscriptions: [AnyCancellable] = []
+    
+    @ObservedObject private var tabsStack: ObservableState<ChildStack<AnyObject, HomeComponentChild>>
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        return tabBarController
+        updateControllers()
+        
+        tabsStack.objectWillChange.sink { [weak self] in
+            DispatchQueue.main.async {
+                self?.updateControllers()
+            }
+        }
+        .store(in: &subscriptions)
     }
     
-    func updateUIViewController(_ navigationController: UITabBarController, context: Context) {
-        context.coordinator.syncChanges(stack, navigationController.viewControllers ?? [])
+    init(
+        tabsStack: ObservableState<ChildStack<AnyObject, HomeComponentChild>>,
+        tabScreen: @escaping (HomeComponentChild) -> HomeTabViewController?,
+        onTabSelected: @escaping (HomeTab) -> Void
+    ) {
+        self.coordinator = HomeTabBarCoordinator(tabScreen: tabScreen, onTabSelected: onTabSelected)
+        self.tabsStack = tabsStack
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        viewControllers = coordinator.createTabViewControllers(stack: stack)
+        delegate = coordinator
+    }
+    
+    @available(*, unavailable) @MainActor dynamic required init?(coder aDecoder: NSCoder) {
+        assertionFailure(DeveloperService.Messages.initHasNotBeenImplemented)
+        
+        return nil
+    }
+    
+    private func updateControllers() {
+        coordinator.syncChanges(stack, viewControllers ?? [])
         
         switch tabsStack.value.active.instance {
         case _ as HomeComponentChild.Tab1:
-            navigationController.selectedIndex = 0
+            selectedIndex = 0
         case _ as HomeComponentChild.Tab2:
-            navigationController.selectedIndex = 1
+            selectedIndex = 1
         case _ as HomeComponentChild.Tab3:
-            navigationController.selectedIndex = 2
+            selectedIndex = 2
         default:
-            navigationController.selectedIndex = 0
+            selectedIndex = 0
         }
     }
+}
+
+class HomeTabBarCoordinator: NSObject, UITabBarControllerDelegate {
+    var tabScreen: (HomeComponentChild) -> HomeTabViewController?
+    var onTabSelected: Closure.Generic<HomeTab>
+    var faked: Set<HomeTab> = []
     
-    private func createTabViewControllers(_ coordinator: HomeTabBarCoordinator) -> [UIViewController] {
+    init(
+        tabScreen: @escaping (HomeComponentChild) -> HomeTabViewController?,
+        onTabSelected: @escaping Closure.Generic<HomeTab>
+    ) {
+        self.tabScreen = tabScreen
+        self.onTabSelected = onTabSelected
+    }
+    
+    func createTabViewControllers(stack: [HomeComponentChild]) -> [UIViewController] {
         var viewControllers: [UIViewController] = []
-        let instances = stack.map { $0.instance }
         
         addViewController(
             viewControllers: &viewControllers,
-            instances: instances,
-            coordinator: coordinator,
+            instances: stack,
             fakeTabBuilder: { HomeComponentChild.Tab1(component: FakeTab1Component()) }
         )
         
         addViewController(
             viewControllers: &viewControllers,
-            instances: instances,
-            coordinator: coordinator,
+            instances: stack,
             fakeTabBuilder: { HomeComponentChild.Tab2(component: FakeTab2Component()) }
         )
         
         addViewController(
             viewControllers: &viewControllers,
-            instances: instances,
-            coordinator: coordinator,
+            instances: stack,
             fakeTabBuilder: { HomeComponentChild.Tab3(component: FakePokemonsComponent()) }
         )
         
         return viewControllers
     }
     
-    private func addViewController<T: HomeComponentChild>(
-        viewControllers: inout [UIViewController],
-        instances: [HomeComponentChild?],
-        coordinator: HomeTabBarCoordinator,
-        fakeTabBuilder: () -> T
-    ) {
-        if let tab = instances.compactMap({ $0 as? T }).first, let controller = tabScreen(tab) {
-            viewControllers.append(controller)
-        } else if let controller = tabScreen(fakeTabBuilder()) {
-            coordinator.faked.insert(controller.homeTab)
-            viewControllers.append(controller)
-        }
-    }
-}
-
-class HomeTabBarCoordinator: NSObject, UITabBarControllerDelegate {
-    var onTabSelected: Closure.Generic<HomeTab>
-    var faked: Set<HomeTab> = []
-    
-    init(onTabSelected: @escaping Closure.Generic<HomeTab>) {
-        self.onTabSelected = onTabSelected
-    }
-    
-    func syncChanges(
-        _ stack: [Child<AnyObject, HomeComponentChild>],
-        _ viewControllers: [UIViewController]
-    ) {
-        let instances = stack.map { $0.instance }
-        
-        update(viewControllers: viewControllers, instances: instances, tab: .tab1, type: HomeComponentChild.Tab1.self)
-        update(viewControllers: viewControllers, instances: instances, tab: .tab2, type: HomeComponentChild.Tab2.self)
-        update(viewControllers: viewControllers, instances: instances, tab: .tab3, type: HomeComponentChild.Tab3.self)
+    func syncChanges(_ stack: [HomeComponentChild], _ viewControllers: [UIViewController]) {
+        update(viewControllers: viewControllers, instances: stack, tab: .tab1, type: HomeComponentChild.Tab1.self)
+        update(viewControllers: viewControllers, instances: stack, tab: .tab2, type: HomeComponentChild.Tab2.self)
+        update(viewControllers: viewControllers, instances: stack, tab: .tab3, type: HomeComponentChild.Tab3.self)
     }
     
     func tabBarController(
@@ -130,7 +147,7 @@ class HomeTabBarCoordinator: NSObject, UITabBarControllerDelegate {
     
     private func update<T: HomeComponentChild>(
         viewControllers: [UIViewController],
-        instances: [HomeComponentChild?],
+        instances: [HomeComponentChild],
         tab: HomeTab,
         type: T.Type
     ) {
@@ -141,6 +158,19 @@ class HomeTabBarCoordinator: NSObject, UITabBarControllerDelegate {
                 .compactMap({ $0 as? HomeTabViewController })
                 .first(where: { $0.homeTab == tab })?
                 .update(component: component)
+        }
+    }
+    
+    private func addViewController<T: HomeComponentChild>(
+        viewControllers: inout [UIViewController],
+        instances: [HomeComponentChild],
+        fakeTabBuilder: () -> T
+    ) {
+        if let tab = instances.compactMap({ $0 as? T }).first, let controller = tabScreen(tab) {
+            viewControllers.append(controller)
+        } else if let controller = tabScreen(fakeTabBuilder()) {
+            faked.insert(controller.homeTab)
+            viewControllers.append(controller)
         }
     }
 }
